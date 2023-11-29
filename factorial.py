bc846d9f0e1af12ea0e65b9e0f4b066de153cbd4
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-29 18:51:10 +0000

    Initial commit, and define factorial problem

---
+++ factorial.py
@@ -0,0 +1,6 @@
+# The mathematical factorial function is defined as the product of integers up to and
+# including the argument.
+# That is, f(x) = f(x - 1) * x, and f(1) = 1
+
+def factorial(n):
+    pass
