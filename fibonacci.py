d6c7b82a957a2c3e60aa89d988de615a7131ddc2
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-29 19:15:00 +0000

    Memoised version

--- fibonacci.py
+++ fibonacci.py
@@ -6,9 +6,25 @@
 # Each subsequent element is the sum of the two preceding elements
 # Write a function that returns the nth Fibonacci number
 
+# Can use memoisation to speed this up
+
+memo_fibonacci = []
+
+
 def fibonacci(n):
+    if len(memo_fibonacci) > n:
+        return memo_fibonacci[n]
     if n == 0:
+        memo_fibonacci.append(0)
         return 0
     if n == 1:
+        memo_fibonacci.append(1)
         return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    sequence_number = fibonacci(n - 1) + fibonacci(n - 2)
+    memo_fibonacci.append(sequence_number)
+    return sequence_number
+
+
+if __name__ == '__main__':
+    result = fibonacci(40)
+    print(result)
