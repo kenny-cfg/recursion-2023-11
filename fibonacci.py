d693455ffd28fb020487bbb38c1c98a2112302a3
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-29 19:02:45 +0000

    Fibonacci problem statement

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+# # FIBONACCI
+
+# Fibonacci series of 5 is : 0 1 1 2 3
+#
+# The 0th and 1st elements are 0 and 1
+# Each subsequent element is the sum of the two preceding elements
+# Write a function that returns the nth Fibonacci number
+
+def fibonacci(n):
+    pass
