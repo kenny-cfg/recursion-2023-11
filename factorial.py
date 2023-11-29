952a7b42703941456fe0d658f1173192c84bfedf
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-29 18:59:26 +0000

    More sparse example

--- factorial.py
+++ factorial.py
@@ -3,4 +3,4 @@
 # That is, f(x) = f(x - 1) * x, and f(1) = 1
 
 def factorial(n):
-    pass
+    return 1 if n == 0 else n * factorial(n - 1)
--- test_factorial.py
+++ test_factorial.py
@@ -1,9 +1,15 @@
 import unittest
 
+from factorial import factorial
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestFactorial(unittest.TestCase):
+    def test_first_seven_factorials_are_correct(self):
+        expected = [1, 1, 2, 6, 24, 120, 720]
+
+        actual = [factorial(x) for x in range(7)]
+
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
