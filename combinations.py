3ed6a29058ee9e17c580e85a19bc0ffca7bbf636
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-29 19:22:48 +0000

    Combinations problem statement

---
+++ combinations.py
@@ -0,0 +1,23 @@
+"""
+Return all possible combinations of strings of given length,
+which can be formed from a set of supplied characters.
+
+Input:
+char_set = {'a', 'b'}, length = 3
+
+Output:
+aaa
+aab
+aba
+abb
+baa
+bab
+bba
+bbb
+
+NB: we cannot use itertools product or permutations functions.
+"""
+
+
+def combinations(characters, length):
+    pass
